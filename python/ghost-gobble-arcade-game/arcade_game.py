fd4cba7cf5845efc8578c94a1be7187f5ea55312
alperenunlu <unlu.alperen@icloud.com>
2022-03-28 16:37:58 +0300

    Add solution of python/ghost-gobble-arcade-game

--- python/ghost-gobble-arcade-game/arcade_game.py
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -1,11 +1,14 @@
+from socket import has_dualstack_ipv6
+
+
 def eat_ghost(power_pellet_active, touching_ghost):
     """
 
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost:  bool - is the player touching a ghost?
-    :return: bool
+    :return: bool - does the player eats a ghost?
     """
-    pass
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -13,9 +16,9 @@ def score(touching_power_pellet, touching_dot):
 
     :param touching_power_pellet: bool - does the player have an active power pellet?
     :param touching_dot:  bool - is the player touching a dot?
-    :return: bool
+    :return: bool - does the player scored?
     """
-    pass
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -23,9 +26,9 @@ def lose(power_pellet_active, touching_ghost):
 
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost: bool - is the player touching a ghost?
-    :return: bool
+    :return: bool - does the player loses?
     """
-    pass
+    return not power_pellet_active and touching_ghost
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -34,6 +37,6 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost:  bool - is the player touching a ghost?
-    :return: bool
+    :return: bool - does the player wins?
     """
-    pass
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)
