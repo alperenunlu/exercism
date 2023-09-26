f28673e7966afa5f96c368ed5d8c0f549a6798ea
alperenunlu <alperenunlu.cs@gmail.com>
2023-09-26 18:27:08 +0300

    Solve python/bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,18 @@
 def response(hey_bob):
-    pass
+
+    hey_bob = hey_bob.strip()
+
+    if not hey_bob:
+        return "Fine. Be that way!"
+
+    elif hey_bob[:-1].isupper() and hey_bob[-1] == "?":
+        return "Calm down, I know what I'm doing!"
+
+    elif hey_bob[-1] == "?":
+        return "Sure."
+    
+    elif hey_bob.isupper():
+        return "Whoa, chill out!"
+
+    else:
+        return "Whatever."
