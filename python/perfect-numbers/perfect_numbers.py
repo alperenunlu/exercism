6c032a5a91175f74245bc7908b23e9e22752e5a3
alperenunlu <alperenunlu.cs@gmail.com>
2023-09-26 18:46:17 +0300

    Solve python/perfect_numbers

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,7 +1,23 @@
+from functools import reduce
+
+def factors(n):    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
 def classify(number):
     """ A perfect number equals the sum of its positive divisors.
 
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number < 1:
+        raise ValueError("Classification is only possible for positive integers.")
+
+    facs_sum = sum(factors(number)) - number
+
+    if facs_sum == number:
+        return "perfect"
+    elif facs_sum > number:
+        return "abundant"
+    else:
+        return "deficient"
