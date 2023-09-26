7b366ab8d323fdb6da570865266d0c5eed707a49
alperenunlu <alperenunlu.cs@gmail.com>
2023-09-26 18:18:59 +0300

    Solve python/collatz_conjecture

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    i = 0
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    
+    while number != 1:
+        if number % 2 == 0:
+            number /= 2
+        else:
+            number *= 3
+            number += 1
+        i += 1
+    return i
