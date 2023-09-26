4151ed80e4fea78ef710ff4061976440c5ef7d79
alperenunlu <alperenunlu.cs@gmail.com>
2023-09-26 18:39:08 +0300

    Solve python/darts

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,11 @@
 def score(x, y):
-    pass
+    cart_dist = (x**2 + y**2)**0.5
+
+    if cart_dist <= 1:
+        return 10
+    elif cart_dist <= 5:
+        return 5
+    elif cart_dist <= 10:
+        return 1
+    else:
+        return 0
