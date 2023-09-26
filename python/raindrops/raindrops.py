00c7904acb2f23e650e1263354b88ddb9e687ea6
alperenunlu <alperenunlu.cs@gmail.com>
2023-09-26 18:33:18 +0300

    Solve python/raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,10 @@
 def convert(number):
-    pass
+    sound = ""
+    if number % 3 == 0: sound += "Pling"
+    if number % 5 == 0: sound += "Plang"
+    if number % 7 == 0: sound += "Plong"
+    if not sound: return str(number)
+    
+    return sound
+
+    
